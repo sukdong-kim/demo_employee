f329d2540a2e4911f8ca8eae9abe448ab7eab07a
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2020-11-15 12:56:28 +0900

    edit box2 class

--- fx-example/src/app2/ViewController2.java
+++ fx-example/src/app2/ViewController2.java
@@ -3,29 +3,32 @@ package app2;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class ViewController2 {
     @FXML
     private HBox hbox;
+    @FXML
+    private VBox vbox;
 
     @FXML
     void onClickb1(ActionEvent event) {
-    	 hbox.setStyle("-fx-background-color: yellow;");
+    	 vbox.setStyle("-fx-background-color: yellow;");
     }
 
     @FXML
     void onClickb2(ActionEvent event) {
-    	 hbox.setStyle("-fx-background-color: green;");
+    	 vbox.setStyle("-fx-background-color: green;");
     }
 
     @FXML
     void onClickb3(ActionEvent event) {
-    	hbox.setStyle("-fx-background-color: brown;");
+    	vbox.setStyle("-fx-background-color: brown;");
     }
     
     @FXML
     void onClickb4(ActionEvent event) {
-   	 hbox.setStyle("-fx-background-color: pink;");
+    	vbox.setStyle("-fx-background-color: pink;");
     }
 
     @FXML
